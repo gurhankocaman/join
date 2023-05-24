2cabbf953ca0d0bdd8f80ea51677c7cd6a1172f1
Luka Stefanovic <lukastefanoviccurioso@gmail.com>
2023-05-24 13:09:15 +0200

    js problem

--- js/add-task.js
+++ js/add-task.js
@@ -15,13 +15,9 @@ async function createTask() {
 
     let title = document.getElementById('titleField');
     let description = document.getElementById('descriptionField');
-    let category = document.getElementById('chooseCategory');
-    let contact = document.getElementById('chooseContact');
-    let date = document.getElementById('dueDateField');
-    let priority = document.getElementById('?????');
-    let subtask = document.getElementById('??????');
+    
 
-    tasks.push({ "title" : title.value, "description" : description.value, "category" : category.value, "contact" : contact.value, "date" : date.value, "priority" : priority.value, "subtask" : subtask.value});
+    tasks.push({ "title" : title.value, "description" : description.value});
 
     await setItem('tasks', JSON.stringify(tasks));
     
