306d2106b1494d0926f959ade00732465c39b857
Björn Daigger <124874016+bjoerndaigger@users.noreply.github.com>
2023-05-24 10:51:20 +0200

    user registration match email

--- js/login.js
+++ js/login.js
@@ -30,7 +30,7 @@ function login() {
         window.location.href = 'summary-user.html';
 
     } else {
-        errorMessage();
+        loginError();
     }
 
     email.value = '';
@@ -40,62 +40,93 @@ function login() {
 // function that checks if remember button was activated while logging in
 async function rememberUser() {
     let rememberUserAsText = localStorage.getItem('rememberUser');
-    
+
     // checks if remember user has a value
     if (rememberUserAsText) {
-      remember = JSON.parse(rememberUserAsText);
-  
-      // get current user
-      let userAsText = localStorage.getItem('currentUser');
-      if (userAsText) {
-        user = JSON.parse(userAsText);
-        username = user['email'];
-        password = user['password'];
-      }
-  
-      // get login status and return email-address
-      if (remember === true) {
-        document.getElementById('email').value = `${username}`;
-        document.getElementById('password').value = `${password}`;
-      }
+        remember = JSON.parse(rememberUserAsText);
+
+        // get current user
+        let userAsText = localStorage.getItem('currentUser');
+        if (userAsText) {
+            user = JSON.parse(userAsText);
+            username = user['email'];
+            password = user['password'];
+        }
+
+        // get login status and return email-address
+        if (remember === true) {
+            document.getElementById('email').value = `${username}`;
+            document.getElementById('password').value = `${password}`;
+        }
     }
-  }
+}
+
+// shows message if email or password is wrong
+function loginError() {
+    let content = document.getElementById('feedback-container');
+    content.innerHTML = '';
+    content.innerHTML += `
+        <div class="danger-text">Wrong email or password. Please try again.</div>
+    `;
+}
 
 function guestLogin() {
     localStorage.setItem('currentUser', '');
     window.location.href = 'summary-user.html';
 }
 
-// shows message if email or password is wrong
-function errorMessage() {
-    let content = document.getElementById('feedback-container');
-    content.innerHTML = '';
-    content.innerHTML += `
-        <div>Wrong email or password. Please try again.</div>
-    `;
+// Register User
+function newUser() {
+    let name = document.getElementById('signup-name').value;
+    let email = document.getElementById('signup-email').value;
+    let password = document.getElementById('signup-password').value;
+    matchEmail(name, email, password);
 }
 
-// Register User
-async function newUser() {
-    let name = document.getElementById('signup-name');
-    let email = document.getElementById('signup-email');
-    let password = document.getElementById('signup-password');
+// Checks if Email is already in use
+function matchEmail(name, email, password) {
+    let found = false;
 
-    users.push({ name: name.value, email: email.value, password: password.value });
+    for (let i = 0; i < users.length; i++) {
+        const newEmail = users[i]['email'];
+        if (newEmail === email) {
+            found = true;
+            break; // stops function if match is found
+        }
+    }
+
+    if (found) {
+        closeSignUpForm();
+        registerError();
+    } else {
+        registerUser(name, email, password);
+    }
+}
+
+async function registerUser(name, email, password) {
+    users.push({ name: name, email: email, password: password });
 
     await setItem('users', JSON.stringify(users));
     closeSignUpForm();
-    successMessage();
+    registrationSuccessful();
 
     name.value = '';
     email.value = '';
     password.value = '';
 }
 
+function registerError() {
+    let content = document.getElementById('feedback-container');
+    content.innerHTML = '';
+    content.innerHTML += `
+        <div class="danger-text">This Email is already registered. Please sign in or try another one.</div>
+    `;
+}
+
 
 
 // shows message if registration was succesfull
-function successMessage() {
+function registrationSuccessful() {
     let content = document.getElementById('feedback-container');
     content.innerHTML = '';
     content.innerHTML += `
@@ -108,6 +139,7 @@ function openSignUpForm() {
     document.getElementById('login-container').classList.add('d-none');
     document.getElementById('login-header-right').classList.add('d-none');
     document.getElementById('signup-container').classList.remove('d-none');
+
 }
 
 
