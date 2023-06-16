f35a692385514decf6bdea99da56b270704ac6fb
Björn Daigger <124874016+bjoerndaigger@users.noreply.github.com>
2023-06-16 20:05:52 +0200

    board css

---
+++ js/board.js
@@ -0,0 +1,10 @@
+function openAddTask() {
+    document.getElementById('add-task-popup').classList.remove('d-none');
+}
+
+
+
+function closePopUp() {
+    document.getElementById('add-task-popup').classList.add('d-none');
+}
+
