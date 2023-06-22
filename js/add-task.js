008696a8973e75c5bd38824ded5848a6945273af
Luka Stefanovic <lukastefanoviccurioso@gmail.com>
2023-06-22 11:09:23 +0200

    prioproblem

--- js/add-task.js
+++ js/add-task.js
@@ -5,23 +5,23 @@ async function createTask() {
 
     addTaskBTN.disabled = true;
 
-    let title = document.getElementById('titleField');
-    let description = document.getElementById('descriptionField');
-
+    var title = document.getElementById('titleField');
+    var description = document.getElementById('descriptionField');
     var category = document.getElementById('chooseCategory');
     var categoryValue = category.options[category.selectedIndex].value;
-
     var contact = document.getElementById('chooseContact');
     var contactValue = contact.options[contact.selectedIndex].value;
-
     var date = document.getElementById('dueDateField');
+    var checkedValue = document.querySelector('.button1:checked').value;
+    var subtask = document.getElementById('subtaskInput');
 
-    var checkedValue = document.querySelector('.messageCheckbox:checked').value;
-    console.log(checkedValue);
-
-    tasks.push({ "title" : title.value, "description" : description.value, "Category" : categoryValue, "AssignedTo" : contactValue, "Date" : date.value});
+    tasks.push({ "title" : title.value, "description" : description.value, "Category" : categoryValue, "AssignedTo" : contactValue, "Date" : date.value, "Priority" : checkedValue, "Subtask" : subtask.value});
 
     await setItem('tasks', JSON.stringify(tasks));
     title.value = '';
     description.value = '';
+    
+    date.value = '';
+
+    subtask.value = '';
 }
