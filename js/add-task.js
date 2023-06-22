5a86703c752a6d960d533633e9bf8fb8b966adbb
Luka Stefanovic <lukastefanoviccurioso@gmail.com>
2023-06-22 10:41:14 +0200

    prioproblem

--- js/add-task.js
+++ js/add-task.js
@@ -7,9 +7,19 @@ async function createTask() {
 
     let title = document.getElementById('titleField');
     let description = document.getElementById('descriptionField');
-    
 
-    tasks.push({ "title" : title.value, "description" : description.value});
+    var category = document.getElementById('chooseCategory');
+    var categoryValue = category.options[category.selectedIndex].value;
+
+    var contact = document.getElementById('chooseContact');
+    var contactValue = contact.options[contact.selectedIndex].value;
+
+    var date = document.getElementById('dueDateField');
+
+    var checkedValue = document.querySelector('.messageCheckbox:checked').value;
+    console.log(checkedValue);
+
+    tasks.push({ "title" : title.value, "description" : description.value, "Category" : categoryValue, "AssignedTo" : contactValue, "Date" : date.value});
 
     await setItem('tasks', JSON.stringify(tasks));
     title.value = '';
