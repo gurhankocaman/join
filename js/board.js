50841fe47b343962cb24e76507113220a0457af9
Björn Daigger <124874016+bjoerndaigger@users.noreply.github.com>
2023-06-27 20:11:26 +0200

    board prio btns

--- js/board.js
+++ js/board.js
@@ -71,10 +71,30 @@ function generateTasksHTML(task) {
             <div class="margin-bottom-10">
                 <div class="progress-bar"></div>
             </div>
-            <div class="card-user-initials">
-                <div>${getUserInitials(task['assignedTo'])}</div>
+            <div class="card-bottom">
+                <div class="card-user-initials">
+                    <div>${getUserInitials(task['assignedTo'])}</div>
+                </div>
+                <div class="card-prio">${checkCardPrio(task['priority'])}</div>
             </div>
         </div>
+    `;
+}
+
+// Show Prio Images Card
+function checkCardPrio(prio) {
+    let prioImg;
+
+    if (prio === 'Urgent') {
+        prioImg = "../assets/img/prio-urgent.png"
+    }
+    if (prio === 'Medium') {
+        prioImg = "../assets/img/prio-medium.png"
+    }
+    if (prio === 'Low') {
+        prioImg = "../assets/img/prio-low.png"
+    }
+    return /*html*/ `<img src="${prioImg}">
     `;
 }
 
@@ -95,8 +115,8 @@ function generatePopupCardHTML(i) {
         <div class="margin-bottom-25">
             <b>Due date:</b> ${tasks[i]['date']}
         </div>
-        <div class="margin-bottom-25">
-            <b>Priority:</b> ${tasks[i]['priority']}
+        <div class="popup-card-prio-container margin-bottom-25">
+            <b>Priority:</b> ${checkPopupCardPrio(tasks[i]['priority'])}
         </div>
         <div>
             <b>Assigned To:</b>
@@ -121,6 +141,33 @@ function generatePopupCardHTML(i) {
     </div>
     `;
 }
+
+// Show Prio Images Card
+function checkPopupCardPrio(prio) {
+    let prioImg;
+    let prioText;
+    let prioColor;
+
+    if (prio === 'Urgent') {
+        prioText = 'Urgent';
+        prioImg = "../assets/img/prio-urgent-white.png";
+        prioColor = "#FF3D00";
+    }
+    if (prio === 'Medium') {
+        prioText = 'Medium';
+        prioImg = "../assets/img/prio-medium-white.png";
+        prioColor = "#FFA800";
+    }
+    if (prio === 'Low') {
+        prioText = 'Low';
+        prioImg = "../assets/img/prio-low-white.png"
+        prioColor = "#7AE229"; 
+
+    }
+    return /*html*/ `<div class="popup-card-prio-btn" style="background-color:${prioColor};"><span>${prioText}</span> <img src="${prioImg}"></div>
+    `;
+}
+
 
 // Getting name initials
 function getUserInitials(username) {
