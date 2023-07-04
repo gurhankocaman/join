fdb0e4bd3abf318558c7d71ae61180b28df70338
Björn Daigger <124874016+bjoerndaigger@users.noreply.github.com>
2023-07-04 09:42:16 +0200

    fixed checkbox saving

--- js/board.js
+++ js/board.js
@@ -139,101 +139,132 @@ function checkCardPrio(prio) {
 
 
 // popup card
-function generatePopupCardHTML(i) {
+function generatePopupCardHTML(taskIndex) {
     let content = document.getElementById('popup-card');
     content.innerHTML = '';
     content.innerHTML += /*html*/ `
-    <div>
-        <div class="close-popup-card" onclick="closePopupCard()">
-            <img src="./assets/img/close-btn.png">
-         </div>
-        <div class="popup-card-category margin-bottom-25">${tasks[i]['category']}</div>
-        <div class="popup-card-title margin-bottom-25">
-            <h2>${tasks[i]['title']}</h2>
-        </div>
-        <div class="margin-bottom-25">${tasks[i]['description']}</div>
-        <div class="margin-bottom-25">
-            <b>Due date:</b> ${tasks[i]['date']}
-        </div>
-        <div class="popup-card-prio-container margin-bottom-25">
-            <b>Priority:</b> ${checkPopupCardPrio(tasks[i]['priority'])}
-        </div>
-        <div class="margin-bottom-25">
-            <b>Assigned To:</b>
-            <div class="popup-card-assigned-to-container">
-                <div class="popup-card-user-initials">
-                <div>${getUserInitials(tasks[i]['assignedTo'])}</div>
-                </div>
-                <div>
-                    ${tasks[i]['assignedTo']}
-                </div>
-            </div>
-        </div>
-        <div class="margin-bottom-25">
-                    <b>Subtasks:</b>
-                    <div id="popup-card-subtasks">
-                </div>           
-        </div>
-        <div class="popup-card-btns">
-            <div onclick="deleteTask(${i})" class="delete-btn">
-                <img src="./assets/img/delete-button.png">
-            </div>
-            <div class="edit-btn">
-                <img src="./assets/img/edit-pencil.png">
-            </div>
-        </div>
-    </div>
+      <div>
+          <div class="close-popup-card" onclick="closePopupCard()">
+              <img src="./assets/img/close-btn.png">
+          </div>
+          <div class="popup-card-category margin-bottom-25">${tasks[taskIndex]['category']}</div>
+          <div class="popup-card-title margin-bottom-25">
+              <h2>${tasks[taskIndex]['title']}</h2>
+          </div>
+          <div class="margin-bottom-25">${tasks[taskIndex]['description']}</div>
+          <div class="margin-bottom-25">
+              <b>Due date:</b> ${tasks[taskIndex]['date']}
+          </div>
+          <div class="popup-card-prio-container margin-bottom-25">
+              <b>Priority:</b> ${checkPopupCardPrio(tasks[taskIndex]['priority'])}
+          </div>
+          <div class="margin-bottom-25">
+              <b>Assigned To:</b>
+              <div class="popup-card-assigned-to-container">
+                  <div class="popup-card-user-initials">
+                  <div>${getUserInitials(tasks[taskIndex]['assignedTo'])}</div>
+                  </div>
+                  <div>
+                      ${tasks[taskIndex]['assignedTo']}
+                  </div>
+              </div>
+          </div>
+          <div class="margin-bottom-25">
+              <b>Subtasks:</b>
+              <div id="popup-card-subtasks-${taskIndex}">
+              </div>
+          </div>
+          <div class="popup-card-btns">
+              <div onclick="deleteTask(${taskIndex})" class="delete-btn">
+                  <img src="./assets/img/delete-button.png">
+              </div>
+              <div class="edit-btn">
+                  <img src="./assets/img/edit-pencil.png">
+              </div>
+          </div>
+      </div>
     `;
-    generateSubtasks(i, tasks[i].subtask);
-}
-
-
-function generateSubtasks(taskId, subtasks) {
-    let content = document.getElementById('popup-card-subtasks');
+    generateSubtasks(taskIndex, tasks[taskIndex].subtask);
+  }
+  
+  async function generateSubtasks(taskIndex, subtasks) {
+    let content = document.getElementById(`popup-card-subtasks-${taskIndex}`);
     content.innerHTML = '';
   
     for (let i = 0; i < subtasks.length; i++) {
-      let isChecked = getCheckboxValue(taskId, i); // Abrufen des gespeicherten Werts
-      content.innerHTML += /*html*/`
-        <input type="checkbox" id="subtask${i}" onchange="submitCheckboxValue(${taskId}, ${i})" ${isChecked ? 'checked' : ''}>
-        <label for="subtask${i}">${subtasks[i]}</label><br>
+      const checkboxId = `subtask-${taskIndex}-${i}`;
+  
+      // Überprüfen, ob der Checkbox-Wert im Remote Storage gespeichert ist
+      const storedValue = await getItem(`task_${taskIndex}_subtask_${i}`);
+      const isChecked = storedValue === 'true'; // Wert in einen booleschen Wert umwandeln
+  
+      content.innerHTML += /*html*/ `
+        <input type="checkbox" id="${checkboxId}" onchange="submitCheckboxValue(${taskIndex}, ${i})" ${isChecked ? 'checked' : ''}>
+        <label for="${checkboxId}">${subtasks[i]}</label><br>
       `;
     }
   }
   
-  function submitCheckboxValue(taskId, subtaskId) {
-    let checkbox = document.getElementById(`subtask${subtaskId}`);
-    let isChecked = checkbox.checked;
-    console.log('isChecked : ' + isChecked);
-    console.log(taskId, subtaskId, isChecked);
-    saveCheckboxValue(taskId, subtaskId, isChecked);
+  async function submitCheckboxValue(taskIndex, checkboxIndex) {
+    const checkboxId = `subtask-${taskIndex}-${checkboxIndex}`;
+    const checkbox = document.getElementById(checkboxId);
+    const isChecked = checkbox.checked;
+    await saveCheckboxValue(taskIndex, checkboxIndex, isChecked);
   }
   
-  async function saveCheckboxValue(taskId, subtaskId, isChecked) {
-    let key = `task_${taskId}_subtask_${subtaskId}`;
+  async function saveCheckboxValue(taskIndex, subtaskId, isChecked) {
+    let key = `task_${taskIndex}_subtask_${subtaskId}`;
     let saveCheckboxValue = isChecked;
-    console.log('saveCheckboxValue = isChecked : ' + saveCheckboxValue);
     await setItem(key, JSON.stringify(saveCheckboxValue));
   }
   
-  async function getCheckboxValue(taskId, subtaskId) {
-    let key = `task_${taskId}_subtask_${subtaskId}`;
-    let value = JSON.parse(await getItem(key));
-    console.log('getCheckboxValue : ' + value);
-    // returned nur, wenn der mit getItem aufgerufene Wert True ist, ansonsten wird false returned
-    return value === 'true';
-  } 
 
-  /* function saveCheckboxValue(taskId, subtaskId, isChecked) {
-    let key = `task_${taskId}_subtask_${subtaskId}`;
-    localStorage.setItem(key, isChecked);
-  }
-  
-  function getCheckboxValue(taskId, subtaskId) {
-    let key = `task_${taskId}_subtask_${subtaskId}`;
-    let value = localStorage.getItem(key);
-    return value === 'true'; 
-  } */
+/* 
+function generateSubtasks(taskId, subtasks) {
+  let content = document.getElementById('popup-card-subtasks');
+  content.innerHTML = '';
+ 
+  for (let i = 0; i < subtasks.length; i++) {
+    let isChecked = getCheckboxValue(taskId, i); // Abrufen des gespeicherten Werts
+    content.innerHTML += `
+    <input type="checkbox" id="subtask${i}" onchange="submitCheckboxValue(${taskId}, ${i})" ${isChecked ? 'checked' : ''}>
+    <label for="subtask${i}">${subtasks[i]}</label><br>
+  `;
+}
+}
+function submitCheckboxValue(taskIndex, subtaskId) {
+  let checkbox = document.getElementById(`subtask${subtaskId}`);
+  let isChecked = checkbox.checked;
+  console.log('isChecked : ' + isChecked);
+  console.log(taskIndex, subtaskId, isChecked);
+  saveCheckboxValue(taskIndex, subtaskId, isChecked);
+}
+ 
+async function saveCheckboxValue(taskIndex, subtaskId, isChecked) {
+  let key = `task_${taskIndex}_subtask_${subtaskId}`;
+  let saveCheckboxValue = isChecked;
+  console.log('saveCheckboxValue = isChecked : ' + saveCheckboxValue);
+  await setItem(key, JSON.stringify(saveCheckboxValue));
+}
+ 
+async function getCheckboxValue(taskIndex, subtaskId) {
+  let key = `task_${taskIndex}_subtask_${subtaskId}`;
+  let value = JSON.parse(await getItem(key));
+  console.log('getCheckboxValue : ' + value);
+  // returned nur, wenn der mit getItem aufgerufene Wert True ist, ansonsten wird false returned
+  return value === 'true';
+} /*
+
+/* function saveCheckboxValue(taskIndex, subtaskId, isChecked) {
+  let key = `task_${taskId}_subtask_${subtaskId}`;
+  localStorage.setItem(key, isChecked);
+}
+ 
+function getCheckboxValue(taskIndex, subtaskId) {
+  let key = `task_${taskId}_subtask_${subtaskId}`;
+  let value = localStorage.getItem(key);
+  return value === 'true'; 
+} */
 
 function deleteTask(i) {
     tasks.splice(i, 1);
@@ -295,3 +326,5 @@ function openAddTask() {
 function closePopUp() {
     document.getElementById('add-task-popup').classList.add('d-none');
 }
+
+
