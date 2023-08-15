96fc40ef26ac64043b4006cbad6969337d89c4cf
Björn Daigger <124874016+bjoerndaigger@users.noreply.github.com>
2023-08-15 17:27:52 +0200

    reset add task

--- js/add-task.js
+++ js/add-task.js
@@ -1,11 +1,11 @@
- function clearBackend() {
+function clearBackend() {
     categories = [];
     categoryColors = [];
     subtasks = [];
     setItem('categories', JSON.stringify(categories));
     setItem('categoryColors', JSON.stringify(categoryColors));
     setItem('subtasks', JSON.stringify(subtasks));
-  } 
+}
 
 let tasks = [];
 let categories = [];
@@ -13,7 +13,7 @@ let subtasks = [];
 let subtaskValues = [];
 let contactValues = [];
 let categoryColors = [];
-let selectedCategoryColor = "#ffffff";
+let selectedCategoryColor = "#CCCCCC";
 
 // Init 
 async function initAddTask() {
@@ -23,7 +23,7 @@ async function initAddTask() {
     await loadSubtasks();
     await loadCategoryColors();
     setCategoryOptions();
-    renderSubtasks(); 
+    renderSubtasks();
     setContactOptions();
 }
 
@@ -59,7 +59,7 @@ async function createTask() {
     const categoryValue = category.options[category.selectedIndex].value;
     const date = document.getElementById('dueDateField').value;
     const checkedValue = document.querySelector('.button1:checked').value;
-    const subtask = document.getElementById('subtaskInput').value;
+    // const subtask = document.getElementById('subtaskInput').value;
 
     // Verarbeite die Subtasks
     subtasksToArray();
@@ -86,31 +86,32 @@ async function createTask() {
     tasks.push(newTask);
     await setItem('tasks', JSON.stringify(tasks));
 
-    // Setze die Eingabefelder und Optionen zurück
-    document.getElementById('titleField').value = '';
-    document.getElementById('descriptionField').value = '';
-    document.getElementById('prio-button').checked = false;
-    document.getElementById('dueDateField').value = '';
-    document.getElementById('subtaskInput').value = '';
-    resetCategoryOptions();
-    resetContactOptions();
-    resetSubtasks();
-
     // Aktiviere den "Add Task" Button wieder
     addTaskBTN.disabled = false;
+
+      // Setze die Eingabefelder und Optionen zurück
+      document.getElementById('titleField').value = '';
+      document.getElementById('descriptionField').value = '';
+      document.getElementById('dueDateField').value = '';
+      document.getElementById('subtaskInput').value = '';
+      resetCategoryOptions();
+      resetContactOptions();
+      resetSubtasks();
+      resetPriorityButtons();
+      selectContact();
 }
 
-function subtasksToArray(){
+function subtasksToArray() {
     var subtasks = document.querySelectorAll("#subtaskList input[type='checkbox']:checked");
-    subtasks.forEach(function(input) {
-      subtaskValues.push({name: input.name, checked: false});
+    subtasks.forEach(function (input) {
+        subtaskValues.push({ name: input.name, checked: false });
     });
 }
 
 function resetCategoryOptions() {
     let categoryOptions = document.getElementById("chooseCategory");
     categoryOptions.selectedIndex = 0;
- }
+}
 
 function resetContactOptions() {
     let contactOptions = document.getElementById("chooseContact");
@@ -119,13 +120,21 @@ function resetContactOptions() {
 
 function resetSubtasks() {
     subtasks = [];
+    subtaskValues = []
     setItem('subtasks', JSON.stringify(subtasks));
+    renderSubtasks();
+}
+
+function resetPriorityButtons() {
+    const priorityButtons = document.querySelectorAll('.button1');
+    for (const button of priorityButtons) {
+        button.checked = false;
+    }
 }
 
 // Categories
-async function setCategoryOptions(){
-    await loadCategories();
-    await loadCategoryColors();
+function setCategoryOptions() {
+
     let categorySelectBox = document.getElementById('chooseCategory');
     for (let i = 0; i < categories.length; i++) {
         categorySelectBox.innerHTML += `<option value="${categories[i]['category']}">${categories[i]['category']}</option>`;
@@ -136,46 +145,47 @@ async function setCategoryOptions(){
 function addCategory() {
     var selectElement = document.getElementById("chooseCategory");
     var selectedValue = selectElement.value;
-    
+
     if (selectedValue === "NewCategory") {
         selectToInput();
     }
-  }
+}
 
-function selectToInput(){
+function selectToInput() {
     let selectToInput = document.getElementById('selectToInput');
-    selectToInput.innerHTML = `
+    selectToInput.innerHTML = /*html*/`
     <div id="subtaskField">
         <div>
             <input id="newCategoryInput" type="text" placeholder="New Category">
             <div class="categoryColor" id="selectedCategoryColor"></div>
             <img onclick="addNewCategory()" src="../assets/img/plus.png">
-        </div>                  
-    </div>`
+        </div>
+    </div>`;
     let element = document.getElementById("categoryColors");
     element.classList.remove("d-none");
-
 }
 
 function selectCategoryColor(color) {
-    selectedCategoryColor = color; // Aktualisiere die globale Variable
+    selectedCategoryColor = color; 
     let selectedColorDiv = document.getElementById('selectedCategoryColor');
     selectedColorDiv.style.backgroundColor = color;
 }
 
 async function addNewCategory() {
-    var category = document.getElementById('newCategoryInput');
-    
-    categories.push({"category" : category.value});
-    categoryColors.push({ "color": selectedCategoryColor }); // Füge die Farbe zur Kategorie hinzu
-    await setItem('categories', JSON.stringify(categories));
-    await setItem('categoryColors', JSON.stringify(categoryColors));
-    resetSelect();
+    let category = document.getElementById('newCategoryInput');
+
+    if (category.value.trim() !== "") {
+        categories.push({ "category": category.value });
+        categoryColors.push({ "color": selectedCategoryColor }); 
+        await setItem('categories', JSON.stringify(categories));
+        await setItem('categoryColors', JSON.stringify(categoryColors));
+        resetSelect();
+    }
 }
 
-function resetSelect(){
+function resetSelect() {
     let selectToInput = document.getElementById('selectToInput');
-    selectToInput.innerHTML = `
+    selectToInput.innerHTML = /*html*/`
     <select name="" id="chooseCategory" class="chooseContact" onchange="addCategory()">
         <option selected disabled>Choose Category</option>
         <option value="NewCategory">New Category</option>
@@ -183,50 +193,66 @@ function resetSelect(){
     var element = document.getElementById("categoryColors");
     element.classList.add("d-none");
     setCategoryOptions();
-    
+
 }
 
 // Contacts 
-async function setContactOptions(){
-    await loadContacts();
+function setContactOptions() {
     let contactSelectBox = document.getElementById('chooseContact');
+
     for (let i = 0; i < contacts.length; i++) {
-        contactSelectBox.innerHTML += `<option value="${contacts[i]['id']}">${contacts[i]['firstName'] + ' ' + contacts[i]['lastName']}</option>`; 
+        contactSelectBox.innerHTML += /*html*/`
+            <option value="${contacts[i]['id']}">${contacts[i]['firstName'] + ' ' + contacts[i]['lastName']}</option>
+        `;
     }
 }
 
 function selectContact() {
     var contact = document.getElementById('chooseContact');
-    var contactValue = parseInt(contact.options[contact.selectedIndex].value); // Wandelt den Wert in eine Zahl um
-    contactValues.push({"id" : contactValue});
+    var contactValue = parseInt(contact.options[contact.selectedIndex].value);
 
-    let contactList = document.getElementById('contactList');
-    contactList.innerHTML = '';
-    for (let i = 0; i < contactValues.length; i++) {
-        const contact = contacts.find(c => c.id == contactValues[i]["id"]);
-        contactList.innerHTML += `<li>${contact.firstName + " " + contact.lastName}</li>`
+    if (contactValue !== -1 && !isContactSelected(contactValue)) {
+        contactValues.push({ "id": contactValue });
+
+        let contactList = document.getElementById('contactList');
+        contactList.innerHTML = '';
+        for (let i = 0; i < contactValues.length; i++) {
+            const selectedContact = contacts.find(c => c.id == contactValues[i]["id"]);
+            contactList.innerHTML += `<li>${selectedContact.firstName + " " + selectedContact.lastName}</li>`;
+        }
     }
-    contact.selectedIndex = 0;
+
+    resetContactOptions();
+}
+
+function isContactSelected(contactId) {
+    return contactValues.some(contact => contact.id === contactId);
 }
 
 // Subtasks
-async function addNewSubtask(){
-    var subtask = document.getElementById('subtaskInput');
-    
-    subtasks.push({"subtask" : subtask.value});
-    await setItem('subtasks', JSON.stringify(subtasks));
-    subtask.value = '';
-    renderSubtasks();
-}
+async function addNewSubtask() {
+    let subtask = document.getElementById('subtaskInput');
 
-async function renderSubtasks(){
-    await loadSubtasks();
-    let subtaskList = document.getElementById('subtaskList');
-    subtaskList.innerHTML = ''
-    for (let i = 0; i < subtasks.length; i++) {
-        subtaskList.innerHTML += `<li><input type="checkbox" name="${subtasks[i]['subtask']}" checked>${subtasks[i]['subtask']}</li>`
+    if (subtask.value.trim() !== "") {
+        subtasks.push({ "subtask": subtask.value });
+        await setItem('subtasks', JSON.stringify(subtasks));
+        subtask.value = '';
+        renderSubtasks();
     }
 }
+
+function renderSubtasks() {
+    let subtaskList = document.getElementById('subtaskList');
+    subtaskList.innerHTML = '';
+    for (let i = 0; i < subtasks.length; i++) {
+        subtaskList.innerHTML += /*html*/ `
+            <li>
+                 <input type="checkbox" name="${subtasks[i]['subtask']}" checked>${subtasks[i]['subtask']}
+            </li>
+        `;
+    }
+}
+
 
 
 
